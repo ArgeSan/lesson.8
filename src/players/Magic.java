7ccd44a03d205eee150d7bcd9240664f068f9502
ArgeSan <129587675+ArgeSan@users.noreply.github.com>
2023-04-27 11:37:20 +0600

    Initial commit

---
+++ src/players/Magic.java
@@ -0,0 +1,19 @@
+package players;
+
+import general.RPG_Game;
+
+public class Magic extends Hero {
+    public Magic(int health, int damage, String name) {
+        super(health, damage, name, SuperAbility.BOOST);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int boost = RPG_Game.random.nextInt(15) + 1;
+        for (int i = 0; i < heroes.length; i++) {
+            heroes[i].setDamage(heroes[i].getDamage() + boost);
+
+        }
+        System.out.println(this.getName() + " increased the hero's attack by " + boost);
+    }
+}
